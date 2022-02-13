b940734bcf9de252336d9d77c85dabb8e1bcf6f6
Guilherme Reis Sousa <96916435+GuilhermeReisSousa@users.noreply.github.com>
2022-02-13 17:07:32 -0300

    Create funcoes.js

---
+++ Material/JavaScript.Material/JavaScript - Basico/Funcoes/funcoes.js
@@ -0,0 +1,12 @@
+function media(n1, n2){
+let nota1 = n1;
+let nota2 = n2;
+let media = (nota1 + nota2) / 2;
+// console.log(media);
+
+return media;
+}
+var res1 = media(6, 7);
+var res2 = media(8, 5);
+
+console.log(`A Media é: ${res1} e ${res2}`);
