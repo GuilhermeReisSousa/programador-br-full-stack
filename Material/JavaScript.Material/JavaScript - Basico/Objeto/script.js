a723cec3836b9dc90be85e9b334312e316bb8ff7
Guilherme Reis Sousa <96916435+GuilhermeReisSousa@users.noreply.github.com>
2022-02-15 13:52:35 -0300

    estudando

---
+++ Material/JavaScript.Material/JavaScript - Basico/Objeto/script.js
@@ -0,0 +1,16 @@
+// var aluno = {
+//     nome: 'Guilherme', 
+//     notas: [7.5, 5.0]
+// }
+
+// var LestName = 'sobrenome';
+
+// aluno.matricula = 12345;
+// aluno['LestName'] = 'Reis'
+// console.log(aluno)
+
+var aluno = new Object();
+aluno.nome = 'Guilherme'
+aluno,LestName = 'Reis'
+aluno.nota = 0.5
+console.log(aluno)
