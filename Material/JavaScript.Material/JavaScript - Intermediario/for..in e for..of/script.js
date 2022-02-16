1a744d759c80897c4b4b5887cb82513cdf7c7382
Guilherme Reis Sousa <96916435+GuilhermeReisSousa@users.noreply.github.com>
2022-02-16 18:23:55 -0300

    Update script.js

--- Material/JavaScript.Material/JavaScript - Intermediario/for..in e for..of/script.js
+++ Material/JavaScript.Material/JavaScript - Intermediario/for..in e for..of/script.js
@@ -1,12 +1,12 @@
 var nome = ['guilherme', 'ilza', 'jose', 'gustovo']
 
-for(var x of nome){
-    console.log(x)
-} // usamos no mundo real
-
-var nome = ['guilherme', 'ilza', 'jose', 'gustovo']
-
 for(var x in nome){
     console.log(x)
 }// esse FOR IN nao e confiavel 
 // quase nao usamos no vida real
+
+var nome = ['guilherme', 'ilza', 'jose', 'gustovo']
+
+for(var x of nome){
+    console.log(x)
+}// usamos no mundo real
