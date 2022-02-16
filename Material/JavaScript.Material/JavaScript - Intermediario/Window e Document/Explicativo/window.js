07773dc71d82ee5f2aff166b5002fd808931dda1
Guilherme Reis Sousa <96916435+GuilhermeReisSousa@users.noreply.github.com>
2022-02-16 18:18:33 -0300

    .

--- Material/JavaScript.Material/JavaScript - Intermediario/Window e Document/Explicativo/window.js
+++ Material/JavaScript.Material/JavaScript - Intermediario/Window e Document/Explicativo/window.js
@@ -2,13 +2,4 @@
 //Podemos brincar no console do google, e bem melhor do que no vscode 
 //Window tem muitos metodos, alguns nao precisamos chamar o Window. podemos so colocar o metodo direto ex:
 console.log('Olá Mundo')
-// alert('Olá Mundo')
-
-var aluno = {
-    nome: 'Guilherme',
-    idade:17
-}    
-
-for(descri in aluno){
-    console.log('O Aluno ' + aluno.nome + ' tem a idade ' + aluno.idade + ' anos ')
-}
+alert('Olá Mundo') // so funciona no google 
