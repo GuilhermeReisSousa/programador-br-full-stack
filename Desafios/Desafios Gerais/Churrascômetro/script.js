1690ca77133eb0325fc49c85ec04189cfeeccba3
Guilherme Reis Sousa <96916435+GuilhermeReisSousa@users.noreply.github.com>
2022-02-28 17:14:36 -0300

    Churrascômetro

---
+++ Desafios/Desafios Gerais/Churrascômetro/script.js
@@ -0,0 +1,5 @@
+//Carne - 400g pro pessoa + de 6 horas - 650
+//Cerveja - 1200ml por pessoa + de  6 horas - 2000 ml
+//Refrigerente / água - 1000 ml pro pessoas + de 6 horas 1500 ml
+
+// crianças valem por 0,5
