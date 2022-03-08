c3a471d900556713040f8c5311eeed10ed07ae9a
Guilherme Reis Sousa <96916435+GuilhermeReisSousa@users.noreply.github.com>
2022-03-08 13:06:42 -0300

    estudo

--- Material/HTML.Material/HTML - Avançado/Videos e iFrame/Video 02/script.js
+++ Material/HTML.Material/HTML - Avançado/Videos e iFrame/Video 02/script.js
@@ -1,39 +1,27 @@
 var video = document.getElementById("video1");
+var controle = document.getElementsByClassName("controle");
 
 function retroceder() {
-
-
-
+  video.currentTime -= 15;
 }
 
 function avancar() {
-
-
-
+  video.currentTime += 15;
 }
 
 function diminuir_v() {
-
-
-
+  video.playbackRate -= 0.25;
 }
 
 function aumentar_v() {
-
-
-
+  video.playbackRate += 0.25;
 }
 
-function play () {
-
-    video.play();
-
+function play() {
+  video.play();
 }
 
 function pausar() {
-
-    video.onpause();
-    video.currentTime = 0;
-
+  video.onpause();
+  video.currentTime = 0;
 }
-
