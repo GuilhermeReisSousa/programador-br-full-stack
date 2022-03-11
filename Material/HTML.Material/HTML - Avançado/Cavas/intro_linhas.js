ce8daba07bddc23fbc2a7dbb8bd6ef8274943425
Guilherme Reis Sousa <96916435+GuilhermeReisSousa@users.noreply.github.com>
2022-03-11 19:30:21 -0300

    Update intro_linhas.js

--- Material/HTML.Material/HTML - Avançado/Cavas/intro_linhas.js
+++ Material/HTML.Material/HTML - Avançado/Cavas/intro_linhas.js
@@ -3,5 +3,8 @@ let tela = document.getElementById("tela");
 let ctx = tela.getContext("2d");
 
 ctx.moveTo(0, 0);
-ctx.lineTo(500, 500);
+ctx.lineTo(250, 250);
+ctx.lineWidth = 3;
+ctx.lineTo(500, 0)
+ctx.strokeStyle = "blue"
 ctx.stroke();
