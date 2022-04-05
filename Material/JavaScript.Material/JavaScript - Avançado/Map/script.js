ff24189c0eccedacb2ef45c427141f3ecbab5a34
Guilherme Reis Sousa <96916435+GuilhermeReisSousa@users.noreply.github.com>
2022-04-05 16:28:06 -0300

    JS

--- Material/JavaScript.Material/JavaScript - Avançado/Map/script.js
+++ Material/JavaScript.Material/JavaScript - Avançado/Map/script.js
@@ -1,16 +1,26 @@
-function novoAluno(nome, idade) {
-  return { nome, idade };
-}
+// function novoAluno(nome, idade) {
+//   return { nome, idade };
+// }
 
-let alunos = [
-  novoAluno("Maria", 23),
-  novoAluno("José", 45),
-  novoAluno("Marcia", 29),
-  novoAluno("João", 35),
-];
+// let alunos = [
+//   novoAluno("Maria", 23),
+//   novoAluno("José", 45),
+//   novoAluno("Marcia", 29),
+//   novoAluno("João", 35),
+// ];
 
-function nomeIdade(aluno) {
-  return aluno.nome + " tem " + aluno.idade + " anos ";
-}
+// function alunoDaquiA5anos(aluno) {
+//   let novoAluno = aluno;
+//   novoAluno.idade += 5;
+//   return novoAluno;
+// }
 
-console.log(alunos.map(nomeIdade));
+// console.log(alunos.map(alunoDaquiA5anos));
+
+let alunoA = {nome: "Guilherme", idade: 15};
+
+let alunoB = {...alunoA}
+
+alunoB.idade = 25;
+
+console.log(alunoB)
