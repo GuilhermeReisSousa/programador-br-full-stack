b8de6331d9ed51743290d7361527c2833325671d
Guilherme Reis Sousa <guilhermereissousa02@gmail.com>
2023-02-24 14:54:26 -0300

    #

--- Material/jQuery/Selecionando/script.js
+++ Material/jQuery/Selecionando/script.js
@@ -1,1 +1,9 @@
-$("ul").find(".item2").hide().show(2000);
+//Jeito certo
+$("#btn1").click(() => {
+  $("ul").find("[criar = item-2]").toggle("slow", "linear");
+});
+
+//Jeito errado
+$("#btn2").click(() => {
+  $("ul").find(".item2").toggle("slow", "linear");
+});
