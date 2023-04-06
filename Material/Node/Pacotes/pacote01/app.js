7036aadb82dfa387502e225d034ba18d60b6775e
Guilherme Reis Sousa <guilhermedeveloperbr9@gmail.com>
2023-04-06 10:59:10 -0300

    #

--- Material/Node/Pacotes/pacote01/app.js
+++ Material/Node/Pacotes/pacote01/app.js
@@ -5,7 +5,7 @@ let width = Number(process.argv[3]);
 function resize(path, width) {
   sharp(path)
     .resize({ width: width })
-    .toFile("../riseze/imagem.jpg", (err) => {
+    .toFile("./imagem.jpg", (err) => {
       if (err) {
         console.log(err);
       } else {
