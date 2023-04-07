94d1cc77140e74c88a31607e5f551641e44e5842
Guilherme Reis Sousa <guilhermedeveloperbr9@gmail.com>
2023-04-07 20:09:12 -0300

    #

--- Material/Node/Pacotes/pacote01/app.js
+++ Material/Node/Pacotes/pacote01/app.js
@@ -1,11 +1,41 @@
 const sharp = require("sharp");
+const compress_images = require("compress-images");
 let path = process.argv[2];
 let width = Number(process.argv[3]);
-function resize(width, path) {}
-sharp(path)
-  .resize({ width: width })
-  .toFile("../new images/New_Image.jpg", (err) => {
-    if (err) throw err;
-    console.log("Sucesso!");
-  });
-resize(width, path);
+
+function resize(width, inputPath, outputPath) {
+  sharp(inputPath)
+    .resize({ width: width })
+    .toFile(outputPath, (err) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log("Sucesso!");
+        compress(output_path, "./compressed/");
+      }
+    });
+}
+
+function compress(pathInput, outputPath) {
+  compress_images(
+    pathInput,
+    outputPath,
+    { compress_force: false, statistic: true, autoupdate: true },
+    false,
+    { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+    { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+    { svg: { engine: "svgo", command: "--multipass" } },
+    {
+      gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] },
+    },
+    function (error, completed, statistic) {
+      console.log("-------------");
+      console.log(error);
+      console.log(completed);
+      console.log(statistic);
+      console.log("-------------");
+    }
+  );
+}
+
+resize(path, "./new-imagems/New_image.jpg", width);
