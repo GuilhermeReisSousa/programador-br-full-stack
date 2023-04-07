5d64863396f471a0263c2275ed353d96e18819ba
Guilherme Reis Sousa <guilhermedeveloperbr9@gmail.com>
2023-04-07 19:14:52 -0300

    #

--- Material/Node/Pacotes/pacote01/app.js
+++ Material/Node/Pacotes/pacote01/app.js
@@ -1,16 +1,11 @@
 const sharp = require("sharp");
 let path = process.argv[2];
 let width = Number(process.argv[3]);
-
-function resize(path, width) {
-  sharp(path)
-    .resize({ width: width })
-    .toFile("./imagem.jpg", (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Imagem redimensionada com sucesso!");
-      }
-    });
-}
-resize(path, width);
+function resize(width, path) {}
+sharp(path)
+  .resize({ width: width })
+  .toFile("./New_Image.jpg", (err) => {
+    if (err) throw err;
+    console.log("Sucesso!");
+  });
+resize(width, path);
