b5d2b16cb7a0ea5f31da1cfed149c0d01ca7ac2b
Guilherme Reis Sousa <guilhermereissousa02@gmail.com>
2023-04-17 12:25:05 -0300

    #

--- Material/Node/HTTP/index.js
+++ Material/Node/HTTP/index.js
@@ -1,14 +1,27 @@
 const http = require("http");
+const url = require("url");
+const fs = require("fs");
 
 http
-  .createServer((resquest, response) => {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.end("<h1>Hello World!</h1>");
+  .createServer((req, resp) => {
+    let path = url.parse(req.url).pathname;
+    let fileName = "." + path;
+
+    fs.readFile(fileName, (err, data) => {
+      if (err) {
+        resp.writeHead(404, { "Content-Type": "text/html" });
+        resp.end("ERRO, nao encontrado");
+      } else {
+        resp.writeHead(200, { "Content-Type": "text/html" });
+        resp.write(data);
+        resp.end();
+      }
+    });
   })
-  .listen(3000, (error) => {
-    if (error) {
-      console.log(error);
+  .listen(3000, (err) => {
+    if (err) {
+      console.log(err);
     } else {
-      console.log("Servidor Iniciado na Porta 3000");
+      console.log("Tudo certo");
     }
   });
