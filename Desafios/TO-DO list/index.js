f97e7ac41f91fae07943f8a2a06d3a3c150e7bff
Guilherme Reis Sousa <guilhermereissousa02@gmail.com>
2023-05-30 20:59:40 -0300

    #

--- Desafios/TO-DO list/index.js
+++ Desafios/TO-DO list/index.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 
 //Para garantir que um chamda a api não busque por uma pasta chamada api, devemos colocor isso primeiro
 app.use("/api", apiRoute);
-app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
