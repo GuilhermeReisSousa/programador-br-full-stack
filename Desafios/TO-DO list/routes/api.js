67972e72883eca9e96292cb1ad6bad81da3b1400
Guilherme Reis Sousa <guilhermereissousa02@gmail.com>
2023-05-31 14:52:50 -0300

    #

--- Desafios/TO-DO list/routes/api.js
+++ Desafios/TO-DO list/routes/api.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const cors = require("cors");
 const posts = require("../module/posts");
+
+const options = {
+  origin: "http://localhost:3000",
+};
+
+router.use(cors(options));
 
 router.get("/all", (req, res) => {
   res.json(posts.getAll());
