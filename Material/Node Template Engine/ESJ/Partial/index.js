7fbe954266a0a190b368ddba8f41b4c0a1b05f28
Guilherme Reis Sousa <guilhermereissousa02@gmail.com>
2023-06-02 17:50:25 -0300

    #

--- Material/Node Template Engine/ESJ/Partial/index.js
+++ Material/Node Template Engine/ESJ/Partial/index.js
@@ -21,11 +21,15 @@ let usersJS = [
 ];
 
 //app.set usar sempre assim.
-app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("index", { usersHTML: usersJS });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
