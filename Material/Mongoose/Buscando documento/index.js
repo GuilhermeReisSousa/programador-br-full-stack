8d6f0522e1c77afa392877e08d5cba70281581e7
Guilherme Reis Sousa <guilhermereissousa02@gmail.com>
2023-06-22 15:22:56 -0300

    #

--- Material/Mongoose/Buscando documento/index.js
+++ Material/Mongoose/Buscando documento/index.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const app = express();
-const port = 8081;
+const express = require('express')
+const app = express()
+const port = 3000
 const mongoose = require('mongoose')
-const searchRouter = require('./Routers/searchRouter')
+const linkRouter = require('./routers/linkRoute')
 
-mongoose.connect("mongodb://127.0.0.1:27017/newlink", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect('mongodb://127.0.0.1:27017/newlink'), {
+  useUnifiedTopology: true, 
 
-const db = mongoose.connection;
+}
 
-db.on("error", () => {console.log("Error: ")});
+let db = mongoose.connection
 
-db.once("open", () => {});
+db.on("error", ()=> {console.log("Houve um erro")})
+db.on("error", ()=> {console.log("Banco carregado")})
 
-app.use('/', searchRouter)
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use('/', linkRouter)
+
+app.listen(port, () => console.log(port))
